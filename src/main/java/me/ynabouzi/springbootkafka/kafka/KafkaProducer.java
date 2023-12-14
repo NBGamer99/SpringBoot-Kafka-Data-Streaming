2c01cb191c59842df44c0b46ba8037c314053e83
NBGamer99 <nabouziyasser80@gmail.com>
2023-12-14 20:38:46 +0100

    feat: added kafka producer service class

---
+++ src/main/java/me/ynabouzi/springbootkafka/kafka/KafkaProducer.java
@@ -0,0 +1,21 @@
+package me.ynabouzi.springbootkafka.kafka;
+
+import org.slf4j.*;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+		this.kafkaTemplate = kafkaTemplate;
+	}
+
+	public void sendMessage(String message) {
+		LOGGER.info(String.format("#### -> Producing message -> %s", message));
+		kafkaTemplate.send("topic1", message);
+	}
+}
