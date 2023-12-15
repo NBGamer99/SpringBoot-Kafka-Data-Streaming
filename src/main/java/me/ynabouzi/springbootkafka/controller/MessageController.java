fa7cf4abd6cb7f6b1a7fe5606b2b2c7ac9b6d59b
NBGamer99 <nabouziyasser80@gmail.com>
2023-12-15 15:32:58 +0100

    refactor: removed unused imports

--- src/main/java/me/ynabouzi/springbootkafka/controller/MessageController.java
+++ src/main/java/me/ynabouzi/springbootkafka/controller/MessageController.java
@@ -1,15 +1,12 @@
 package me.ynabouzi.springbootkafka.controller;
 
 import me.ynabouzi.springbootkafka.kafka.Producers.KafkaStringProducer;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/kafka")
